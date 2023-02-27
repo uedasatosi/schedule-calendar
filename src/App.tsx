2e345e39e18ec35d77c22a405dd24e2861aa6339
uedasatosi <sato1030ryokutya@gmail.com>
2023-02-27 18:23:00 +0900

    add timeline to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Task } from "@mui/icons-material";
 import { Stack } from "@mui/material";
 import Calender from "./components/Calender";
+import TimeLine from "./components/TimeLine";
 import { ToDo } from "./components/ToDo";
 
 function Home() {
-  return <Stack direction={"row"}><Calender/><ToDo/>
-  </Stack>;
+  return (
+    <Stack direction={"row"}>
+      <Calender />
+      <ToDo />
+      <TimeLine />
+    </Stack>
+  );
 }
 export default Home;
